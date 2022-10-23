cd8c193b525072cf3b4596d9ae7e022915c8ebea
Carlos Pavajeau <cantte098@gmail.com>
2022-10-23 16:51:13 -0500

    feat: add app modules

---
+++ src/app/modules/sales-departments/sales-departments.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SalesDepartmentsRoutingModule } from './sales-departments-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SalesDepartmentsRoutingModule
+  ]
+})
+export class SalesDepartmentsModule { }
